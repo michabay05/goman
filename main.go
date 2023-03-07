0d8bdf557d76bf59748a6f494f19bba1edc5a422
michabay05 <michabay05@gmail.com>
2023-03-07 18:46:10 -0500

    Added randomized hidden words

--- main.go
+++ main.go
@@ -3,10 +3,33 @@ package main
 import (
 	"fmt"
 	"hangman/game"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"regexp"
 )
 
+func get_rand_word(path string) string {
+	content, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatal()
+	}
+	words := string(content)
+	regex := regexp.MustCompile(`\s`)
+	split := regex.Split(words, -1)
+	word_set := []string{}
+
+	for i := range split {
+		word_set = append(word_set, split[i])
+	}
+	rand_ind := rand.Intn(len(word_set))
+	return word_set[rand_ind]
+}
+
 func main() {
-	hangman := game.Hangman_new("happy")
+	hidden_word := get_rand_word("words.txt")
+	hangman := game.Hangman_new(hidden_word)
+	fmt.Println(hidden_word)
 
 	found_word := false
 	for hangman.Has_attempts() {
@@ -25,7 +48,7 @@ func main() {
 		fmt.Println()
 	}
 	if found_word {
-		fmt.Println("Well done! You've found the hidden word.")
+		fmt.Printf("Well done! You've found the hidden word - '%s'.\n", hangman.Get_hidden_word())
 	} else {
 		fmt.Printf("Unfortunately, you have not found the hidden word, which was '%s'\n.", hangman.Get_hidden_word())
 	}
